fba05d5275ba9f562c76f6a9b6a28ceca3522061
Alex.com <bueno.zat@obmontessori.edu.ph>
2026-05-21 14:48:44 +0800

    Implement login functionality in main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+var u, p
+u = "Student";
+p = "Password";
 
+function LogIn() {
+    let uinput = document.getElementById("userN").value;
+    let pinput = document.getElementById("passW").value;
+
+    if (u === uinput && p === pinput) {
+        r = "Welcome back, Student! :D";
+        
+    } else {
+        r = "WRONG!";
+        
+    }
+    document.getElementById("result").innerHTML = r;
+}
