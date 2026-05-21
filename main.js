e319ff748d4cb34d395940a70abe66ff7be81190
Agnote9S-ay2526 <agnote.ac@obmontessori.edu.ph>
2026-05-21 18:33:27 +0800

    main.js

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
-var u, p
+var u, p;
+
 u = "Student";
 p = "Password";
 
 function LogIn() {
+
     let uinput = document.getElementById("userN").value;
     let pinput = document.getElementById("passW").value;
 
     if (u === uinput && p === pinput) {
-        r = "Welcome back, Student! :D";
-        
-    } else {
-        r = "WRONG!";
-        
+
+        document.getElementById("result").innerHTML =
+        "Welcome back, Student! :D";
+
+        window.location.href = "dashboard.html";
     }
-    document.getElementById("result").innerHTML = r;
+
+    else {
+
+        document.getElementById("result").innerHTML =
+        "WRONG!";
+    }
 }
